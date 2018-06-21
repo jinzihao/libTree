a2a0be6b3a1168d2dc443b61e9b161fe349c9b46
jinzihao <903703287@qq.com>
2018-06-21 18:56:41 +0800

    Added 100.h

---
+++ 100.h
@@ -0,0 +1,11 @@
+bool isSameTree(TreeNode* p, TreeNode* q) {
+    if (!p && !q) {
+        return true;
+    }
+    else if (p && q) {
+        return p->val == q->val && isSameTree(p->left, q->left) && isSameTree(p->right, q->right);
+    }
+    else {
+        return false;
+    }
+}
